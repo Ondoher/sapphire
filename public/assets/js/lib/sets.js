3082cb3409ca23a8b1dc1cb417445c751c891cd9
Ondoher <ondohergmail.com>
2015-03-01 13:10:12 -0800

    - had an old, broken implementation of sets.

--- public/assets/js/lib/sets.js
+++ public/assets/js/lib/sets.js
@@ -1,68 +1,71 @@
 
-var CHUNK_SIZE = 24;
-var CHUNK_MASK = 0xffffff;
+var Set = new Class({
+	CHUNK_SIZE : 24,
+	CHUNK_MASK : 0xffffff,
 
-function arrayPad(array, count, value)
-{
-	for (var idx = array.length; idx < count; idx++)
-		array.push(value)
-	return array;
-}
+	initialize : function(bits, length)
+	{
+		this.length = length?length:1024;
+		this.chunkCount = Math.ceil(this.length / this.CHUNK_SIZE);
+		this.chunks = [];
+		this.chunks = this.arrayPad(this.chunks, this.chunkCount, 0);
+		this.assign(bits);
+	},
 
-function Set(bits, length)
-{
-	this.length = length;
-	this.chunkCount = Math.ceil(length / CHUNK_SIZE);
-	this.chunks = [];
-	this.chunks = arrayPad(this.chunks, this.chunkCount, 0);
-	this.assign(bits);
-}
+	arrayPad : function(array, count, value)
+	{
+		for (var idx = array.length; idx < count; idx++)
+			array.push(value)
+		return array;
+	},
 
-Set.prototype.isEmpty =	function()
-{
-	for (var idx = 0; idx < this.chunks.length; idx++)
-		if (this.chunks[idx] != 0) return false;
+	isEmpty : function()
+	{
+		for (var idx = 0; idx < this.chunks.length; idx++)
+			if (this.chunks[idx] != 0) return false;
 
-	return true;
-}
+		return true;
+	},
 
-Set.prototype.assign = function(bits)
-{
-	for (var idx = 0; idx < bits.length; idx++)
-		this.includeBit(bits[idx]);
-}
+	assign : function(bits)
+	{
+		for (var idx = 0; idx < bits.length; idx++)
+			this.includeBit(bits[idx]);
+//		console.log('set::assign', bits, this);
+	},
 
-Set.prototype.includeBit = function(bit)
-{
-	this.chunks[Math.floor(bit / CHUNK_SIZE)] |= (1 << (bit % CHUNK_SIZE));
-}
+	includeBit : function(bit)
+	{
+		this.chunks[Math.floor(bit / this.CHUNK_SIZE)] |= (1 << (bit % this.CHUNK_SIZE));
+	},
 
-Set.prototype.excludeBit = function(bit)
-{
-	this.chunks[Math.floor(bit / CHUNK_SIZE)] &= (~(1 << (bit % CHUNK_SIZE))) & CHUNK_MASK;
-}
+	excludeBit : function(bit)
+	{
+		this.chunks[Math.floor(bit / this.CHUNK_SIZE)] &= (~(1 << (bit % this.CHUNK_SIZE))) & this.CHUNK_MASK;
+	},
 
-Set.prototype.inBit = function(bit)
-{
-	var result = ((this.chunks[Math.floor(bit / CHUNK_SIZE)]) & (1 << (bit % CHUNK_SIZE)))
-	return result;
-}
+	inBit : function(bit)
+	{
+		var result = ((this.chunks[Math.floor(bit / this.CHUNK_SIZE)]) & (1 << (bit % this.CHUNK_SIZE)))
+		//console.log('inBit', bit, result);
+		return result != 0;
+	},
 
-Set.prototype.union = function (set)
-{
-	for (var idx = 0; idx < this.chunks.lenth; idx++)
-		this.chunks[idx] |= set.chunks[idx];
-}
+	union : function (set)
+	{
+		for (var idx = 0; idx < this.chunks.length; idx++)
+			this.chunks[idx] |= set.chunks[idx];
+	},
 
-Set.prototype.intersection = function(set)
-{
-	for (var idx = 0; idx < this.chunks.lenth; idx++)
-		this.chunks[idx] &= set.chunks[$idx];
-}
+	intersection : function(set)
+	{
+		for (var idx = 0; idx < this.chunks.length; idx++)
+			this.chunks[idx] &= set.chunks[$idx];
+	},
 
-Set.prototype.difference = function($set)
-{
-	for (var idx = 0; idx < this.chunks.lenth; idx++)
-		this.chunks[idx] &= (~set.chunks[idx]) & CHUNK_MASK;
-}
-
+	difference : function(set)
+	{
+		for (var idx = 0; idx < this.chunks.length; idx++)
+			this.chunks[idx] &= (~set.chunks[idx]) & this.CHUNK_MASK;
+	}
+});
